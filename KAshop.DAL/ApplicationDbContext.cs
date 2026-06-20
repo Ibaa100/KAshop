6308edfaf4030706b2cc73e489fd417f29643983
Rasmi SayedAhmad <Rasmi SayedAhmad@gmail.com>
2026-06-20 12:24:56 +0300

    Add project files.

---
+++ KAshop.DAL/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace KAshop.DAL
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
---
+++ KAshop.PL/Controllers/UsersController.cs
@@ -0,0 +1,18 @@
+﻿using KAshop.DAL;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace KAshop.PL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+         ApplicationDbContext _context;
+        public UsersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
